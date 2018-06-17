73e9034b5e502040efec31208cd4fd9a85c2c1af
Jonathan Bäckström <jonathan.backstrom95@gmail.com>
2018-06-17 18:37:10 +0200

    Add MainScopeCopy to use for outside call to parseLines

--- LineParser/SyntaxCheck.cs
+++ LineParser/SyntaxCheck.cs
@@ -8,13 +8,16 @@ namespace Compiler
 	{
 
 		public static B83.ExpressionParser.ExpressionParser globalParser;
+		
+		// Temporary copy to use by Kjell for input argument parser
+		public static Scope MainScopeCopy;
 
 		/// <summary>
 		/// Declares globalParser and returns parsed lines in list of Codelines.
 		/// </summary>
 		public static List<CodeLine> parseLines(string currentText){
 			globalParser = new B83.ExpressionParser.ExpressionParser ();
-
+			
 			List<ParsedLine> textLines = LineSplitter.splitTextIntoLines(currentText);
 			return LineParser.parseLines (textLines);
 
@@ -23,6 +26,7 @@ namespace Compiler
 		/// Links fullText and actions (endWalker etc) to CodeWalker via setActions.
 		public static void CompileCode(string fullText, Action endWalker, Action pauseWalker, Action activateFunctionColor, Action<int> setWalkerPos){
 			Scope mainScope = ScopeParser.parseIntoScopes (fullText);
+			MainScopeCopy = mainScope;
 			Runtime.CodeWalker.setActions(endWalker, pauseWalker, activateFunctionColor, setWalkerPos, mainScope);	
 		}
 	}
