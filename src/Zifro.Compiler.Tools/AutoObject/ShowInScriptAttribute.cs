05bc671ef7d64b788572e01b124e5c332af62231
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-01 16:39:50 +0100

    Added basic autoobject to start with

---
+++ src/Zifro.Compiler.Tools/AutoObject/ShowInScriptAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zifro.Compiler.Tools.AutoObject
+{
+    [AttributeUsage(AttributeTargets.Field |
+                    AttributeTargets.Method |
+                    AttributeTargets.Property)]
+    public class ShowInScriptAttribute : Attribute
+    {
+        public string Name;
+    }
+}
---
+++ src/Zifro.Compiler.Tools/Extensions/ScriptTypeFactoryExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zifro.Compiler.Core.Interfaces;
+
+namespace Zifro.Compiler.Tools.Extensions
+{
+    public static class ScriptTypeFactoryExtensions
+    {
+        public static bool TryCreate<T>(this IScriptTypeFactory factory, T clrValue, out IScriptType scriptTypeValue)
+        {
+            switch (clrValue)
+            {
+                case bool v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case int v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case short v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case byte v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case long v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case char v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case string v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case IList<IScriptType> v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case IDictionary<IScriptType, IScriptType> v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                case IFunction v:
+                    scriptTypeValue = factory.Create(v);
+                    return true;
+
+                default:
+                    scriptTypeValue = default;
+                    return false;
+            }
+        }
+    }
+}
