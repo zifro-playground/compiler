ecb15fc67f101a071f50d401bfcea46354755d17
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-01 15:55:08 +0100

    Added IValueType, a value that is value type (non-reference)

---
+++ src/Zifro.Compiler.Core/Interfaces/IValueType.cs
@@ -0,0 +1,10 @@
+﻿namespace Zifro.Compiler.Core.Interfaces
+{
+    public interface IValueType
+    {
+        /// <summary>
+        /// Clones this value. Used for integers, floats, strings, etc.
+        /// </summary>
+        IValueType Clone();
+    }
+}
