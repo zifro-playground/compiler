3b13ea8462956f28c297cb63b75e29f1493f93b9
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-01 15:20:37 +0100

    Added function to returnvalue proxy

--- src/Zifro.Compiler/Proxies/ReturnValue.cs
+++ src/Zifro.Compiler/Proxies/ReturnValue.cs
@@ -4,7 +4,15 @@ namespace Zifro.Compiler.Proxies
 {
     public class ReturnValue : ValueProxyBase
     {
-        public ReturnValue(IValueType innerValue) : base(innerValue)
-        { }
+        /// <summary>
+        /// Gets the function from which this value was retrieved.
+        /// </summary>
+        public IFunctionType Function { get; }
+
+        public ReturnValue(IValueType innerValue, IFunctionType function)
+            : base(innerValue)
+        {
+            Function = function;
+        }
     }
 }
