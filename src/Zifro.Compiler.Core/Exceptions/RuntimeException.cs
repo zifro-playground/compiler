c24aed1407c968dc460e5808b1df7cd13b9febf9
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-04 12:16:16 +0100

    Added so runtime exception stores formatting args

--- src/Zifro.Compiler.Core/Exceptions/RuntimeException.cs
+++ src/Zifro.Compiler.Core/Exceptions/RuntimeException.cs
@@ -7,22 +7,34 @@ namespace Zifro.Compiler.Core.Exceptions
     public class RuntimeException : InterpreterException
     {
         public string LocalizeKey { get; }
+        public object[] FormatArgs { get; }
 
-        public RuntimeException(string localizeKey,
-            string localizedMessage, Exception innerException)
+        private RuntimeException(string localizeKey,
+            string localizedMessage, object[] formatArgs, Exception innerException)
             : base(localizedMessage, innerException)
         {
             LocalizeKey = localizeKey;
+            FormatArgs = formatArgs ?? new object[0];
+        }
+
+        public RuntimeException(string localizeKey,
+            string localizedMessage, Exception innerException)
+            : this(localizeKey, localizedMessage,
+                formatArgs: null, innerException: innerException)
+        {
         }
 
         public RuntimeException(string localizeKey, string localizedMessage)
-            : this(localizeKey, localizedMessage, innerException: null)
+            : this(localizeKey, localizedMessage,
+                formatArgs: null, innerException: null)
         {
         }
 
         public RuntimeException(string localizeKey, string localizedMessageFormat,
             params object[] values)
-            : this(localizeKey, string.Format(localizedMessageFormat, values), innerException: null)
+            : this(localizeKey,
+                string.Format(localizedMessageFormat, values),
+                formatArgs: values, innerException: null)
         {
         }
     }
