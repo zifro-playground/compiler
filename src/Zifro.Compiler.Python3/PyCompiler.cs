b7b9e1f0fe2fdc2e9b4628a84d97b53e9acb44d5
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-05 14:53:44 +0100

    Added python parser via ANTLR 4 to python3 project

---
+++ src/Zifro.Compiler.Python3/PyCompiler.cs
@@ -0,0 +1,13 @@
+﻿using Antlr4.Runtime;
+using Zifro.Compiler.Lang.Python3.AST;
+
+namespace Zifro.Compiler.Lang.Python3
+{
+    public class PyCompiler : Python3BaseVisitor<ParserRuleContext>
+    {
+        public override ParserRuleContext VisitFile_input(Python3Parser.File_inputContext context)
+        {
+            return base.VisitFile_input(context);
+        }
+    }
+}
