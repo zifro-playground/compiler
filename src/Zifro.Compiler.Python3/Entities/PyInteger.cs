8555afa56a6d3adb7d14bbcc646ce4f346dbe483
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-05 10:31:28 +0100

    Added base types as python types

---
+++ src/Zifro.Compiler.Python3/Entities/PyInteger.cs
@@ -0,0 +1,14 @@
+﻿using Zifro.Compiler.Core.Interfaces;
+using Zifro.Compiler.Lang.Entities;
+
+namespace Zifro.Compiler.Lang.Python3.Entities
+{
+    public class PyInteger : IntegerBase
+    {
+        /// <inheritdoc />
+        public override IScriptType GetTypeDef()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Zifro.Compiler.Python3/Entities/PyString.cs
@@ -0,0 +1,14 @@
+﻿using Zifro.Compiler.Core.Interfaces;
+using Zifro.Compiler.Lang.Entities;
+
+namespace Zifro.Compiler.Lang.Python3.Entities
+{
+    public class PyString : StringBase
+    {
+        /// <inheritdoc />
+        public override IScriptType GetTypeDef()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
