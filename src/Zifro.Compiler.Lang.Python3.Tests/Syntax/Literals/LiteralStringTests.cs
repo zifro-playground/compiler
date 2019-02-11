b0e8bcc61f0042decc279a4a0fe09f71be867e05
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-11 19:07:37 +0100

    Added unescaping method

--- src/Zifro.Compiler.Lang.Python3.Tests/Syntax/Literals/LiteralStringTests.cs
+++ src/Zifro.Compiler.Lang.Python3.Tests/Syntax/Literals/LiteralStringTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zifro.Compiler.Core.Entities;
 using Zifro.Compiler.Lang.Python3.Syntax.Literals;
 
@@ -18,6 +19,8 @@ namespace Zifro.Compiler.Lang.Python3.Tests.Syntax.Literals
         [DataRow("'new\\nline'", "new\nline")]
         [DataRow("'foo\\'bar'", "foo'bar")]
         [DataRow("\"foo'bar\"", "foo'bar")]
+        [DataRow("r'foo'", "foo")] // raw
+        [DataRow("r'new\\nline'", "new\\nline")]
         public override void ParseValidTest(string input, string expectedValue)
         {
             base.ParseValidTest(input, expectedValue);
@@ -38,7 +41,6 @@ namespace Zifro.Compiler.Lang.Python3.Tests.Syntax.Literals
         [DataTestMethod]
         [DataRow("\"\"\"foo\"\"\"")]
         [DataRow("'''foo'''")]
-        [DataRow("r'foo'")] // raw
         [DataRow("u'foo'")] // unicode
         [DataRow("b'foo'")] // bytes
         [DataRow("f'foo'")] // formatted
@@ -49,6 +51,26 @@ namespace Zifro.Compiler.Lang.Python3.Tests.Syntax.Literals
         public override void ParseNotYetImplementedTest(string input)
         {
             base.ParseNotYetImplementedTest(input);
+        }
+
+        [DataTestMethod]
+        [DataRow("hello world", "hello world")]
+        [DataRow("new\\nline", "new\nline")]
+        [DataRow("unknown escape \\p", "unknown escape \\p")]
+        [DataRow("\\\\n", "\\n")]
+        [DataRow("\"\'\\\"\\n\\f", "\"'\"\n\f")]
+        [DataRow("\\x42", "\x42")] // hexadecimal
+        [DataRow("\\X42", "\\X42")] // hexadecimal
+        [DataRow("\\123", "S")] // octal
+        [DataRow("\\1", "\x1")] // octal
+        [DataRow("\\1\\123", "\x1S")] // octal
+        public void UnescapeTest(string input, string expected)
+        {
+            // Act
+            string result = LiteralString.Unescape(input);
+
+            // Assert
+            Assert.AreEqual(expected, result, $"\nEscaped expected:<{Regex.Escape(expected)}>\nEscaped actual:<{Regex.Escape(result)}>");
         }
     }
 }
--- src/Zifro.Compiler.Lang.Python3/Syntax/Literals/LiteralString.cs
+++ src/Zifro.Compiler.Lang.Python3/Syntax/Literals/LiteralString.cs
@@ -1,4 +1,8 @@
-﻿using Zifro.Compiler.Core.Entities;
+﻿using System;
+using System.Text;
+using Zifro.Compiler.Core.Entities;
+using Zifro.Compiler.Core.Exceptions;
+using Zifro.Compiler.Lang.Python3.Exceptions;
 
 namespace Zifro.Compiler.Lang.Python3.Syntax.Literals
 {
@@ -11,7 +15,202 @@ namespace Zifro.Compiler.Lang.Python3.Syntax.Literals
 
         public static LiteralString Parse(SourceReference source, string text)
         {
-            throw new System.NotImplementedException();
+            // At least 2 for an empty quoted string
+            if (text.Length < 2)
+            {
+                throw new SyntaxLiteralFormatException(source);
+            }
+
+            string modifiers = GetModifiers();
+            string withoutModifiers = text.Substring(modifiers.Length);
+            string quotes = GetQuotes();
+
+            // Check it's longer than quotes
+            if (withoutModifiers.Length < quotes.Length * 2)
+            {
+                throw new SyntaxLiteralFormatException(source);
+            }
+
+            // Check ending with correct quotes
+            if (withoutModifiers.Substring(withoutModifiers.Length - quotes.Length) != quotes)
+            {
+                throw new SyntaxLiteralFormatException(source);
+            }
+
+            string withoutQuotes = withoutModifiers.Substring(quotes.Length, withoutModifiers.Length - quotes.Length);
+            switch (modifiers.ToLowerInvariant())
+            {
+                case "":
+                    return new LiteralString(source, Unescape(withoutQuotes));
+
+                case "r": // raw
+                    return new LiteralString(source, withoutQuotes);
+
+                case "b": // bytes
+                case "br": // raw+bytes
+                case "rb": // raw+bytes
+                case "f": // formatted
+                case "rf": // raw+formatted
+                case "fr": // raw+formatted
+                case "u": // unicode
+                    throw new SyntaxNotYetImplementedException(source);
+
+                default:
+                    throw new SyntaxLiteralFormatException(source);
+            }
+
+            string GetQuotes()
+            {
+                if (withoutModifiers.Length == 0)
+                    throw new SyntaxLiteralFormatException(source);
+
+                char quote = withoutModifiers[0];
+                var longStringQuotes = new string(quote, 3);
+                return withoutModifiers.StartsWith(longStringQuotes)
+                    ? longStringQuotes
+                    : quote.ToString();
+            }
+
+            string GetModifiers()
+            {
+                var builder = new StringBuilder();
+
+                foreach (char c in text)
+                {
+                    if (c == '"' || c == '\'')
+                        return builder.ToString();
+
+                    builder.Append(c);
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public static string Unescape(string value)
+        {
+            var builder = new StringBuilder(value.Length);
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                char c = value[i];
+                if (c == '\\')
+                {
+                    if (i + 1 >= value.Length)
+                    {
+                        builder.Append(value[i]);
+                        continue;
+                    }
+
+                    if (GetEscapedChar(i + 1, out c))
+                    {
+                        builder.Append(c);
+                        i++;
+                    }
+                    else if (GetOctalChars(i, out string oStr))
+                    {
+                        builder.Append((char) Convert.ToInt32(oStr, 8));
+                        i += oStr.Length;
+                    }
+                    else if (GetHexChars(i, out string xStr))
+                    {
+                        builder.Append((char) Convert.ToInt32(xStr, 16));
+                        i += xStr.Length + 1;
+                    }
+                    else
+                    {
+                        builder.Append(value[i]);
+                        builder.Append(value[++i]);
+                    }
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+
+            return builder.ToString();
+
+            bool GetEscapedChar(int i, out char c)
+            {
+                switch (value[i])
+                {
+                    case '\\': c = '\\'; break;
+                    case '\'': c = '\''; break;
+                    case '"': c = '"'; break;
+                    case 'a': c = '\a'; break;
+                    case 'b': c = '\b'; break;
+                    case 'f': c = '\f'; break;
+                    case 'n': c = '\n'; break;
+                    case 'r': c = '\r'; break;
+                    case 't': c = '\t'; break;
+                    case 'v': c = '\v'; break;
+                    default:
+                        c = default;
+                        return false;
+                }
+                return true;
+            }
+
+            bool GetOctalChars(int i, out string octString)
+            {
+                var b = new StringBuilder(3);
+
+                // Skip the /
+                i++;
+
+                for (; i < value.Length && b.Length < 3; i++)
+                {
+                    char v = value[i];
+                    if (v >= '0' && v <= '7')
+                        b.Append(v);
+                    else
+                        break;
+                }
+
+                if (b.Length > 0)
+                {
+                    octString = b.ToString();
+                    return true;
+                }
+
+                octString = default;
+                return false;
+            }
+
+            bool GetHexChars(int i, out string hexStr)
+            {
+                var b = new StringBuilder(3);
+
+                // Skip the /
+                i++;
+
+                if (value.Length > i && value[i] == 'x')
+                {
+                    // Skip the x
+                    i++;
+
+                    for (; i < value.Length && b.Length < 2; i++)
+                    {
+                        char v = value[i];
+                        if (v >= '0' && v <= '9' ||
+                            v >= 'a' && v <= 'f' ||
+                            v >= 'A' && v <= 'F')
+                            b.Append(v);
+                        else
+                            break;
+                    }
+
+                    if (b.Length > 0)
+                    {
+                        hexStr = b.ToString();
+                        return true;
+                    }
+                }
+
+                hexStr = default;
+                return false;
+            }
         }
     }
 }
