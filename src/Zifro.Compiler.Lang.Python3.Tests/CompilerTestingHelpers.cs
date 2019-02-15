de4b7a67c9a047dc2c337cf19b23759ecaad2b25
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-15 11:31:06 +0100

    Better testing error messages

--- src/Zifro.Compiler.Lang.Python3.Tests/CompilerTestingHelpers.cs
+++ src/Zifro.Compiler.Lang.Python3.Tests/CompilerTestingHelpers.cs
@@ -9,7 +9,7 @@ namespace Zifro.Compiler.Lang.Python3.Tests
         public static void IsPushLiteralOpCode<TValue>(this Assert assert, int expectedValue, PyCompiler compiler, int index)
         {
             if (index >= compiler.Count)
-                throw new AssertFailedException($"Expected IOpCode at index {index} but compiler only contains {compiler.Count} op codes.");
+                throw new AssertFailedException($"Expected PushLiteral<{typeof(TValue).Name}> op code at index {index} but compiler only contains {compiler.Count} op codes.");
 
             IOpCode opCode = compiler[index];
             Assert.IsInstanceOfType(opCode, typeof(PushLiteral<TValue>));
@@ -20,7 +20,7 @@ namespace Zifro.Compiler.Lang.Python3.Tests
         public static void IsBinaryOpCode(this Assert assert, OperatorCode expectedCode, PyCompiler compiler, int index)
         {
             if (index >= compiler.Count)
-                throw new AssertFailedException($"Expected IOpCode at index {index} but compiler only contains {compiler.Count} op codes.");
+                throw new AssertFailedException($"Expected {expectedCode} op code at index {index} but compiler only contains {compiler.Count} op codes.");
 
             IOpCode opCode = compiler[index];
             Assert.IsInstanceOfType(opCode, typeof(BasicOperator));
@@ -32,7 +32,7 @@ namespace Zifro.Compiler.Lang.Python3.Tests
             where TOpCode : IOpCode
         {
             if (index >= compiler.Count)
-                throw new AssertFailedException($"Expected IOpCode at index {index} but compiler only contains {compiler.Count} op codes.");
+                throw new AssertFailedException($"Expected {typeof(TOpCode).Name} op code at index {index} but compiler only contains {compiler.Count} op codes.");
 
             IOpCode opCode = compiler[index];
             Assert.IsInstanceOfType(opCode, typeof(TOpCode), $"Op code at index {index} is wrong type.");
