3da17fc81b7644e25a99fc7b186d23510efe5405
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-15 15:27:29 +0100

    Resolved jumpiffalse units

--- src/Zifro.Compiler.Lang.Python3/Instructions/JumpIfFalse.cs
+++ src/Zifro.Compiler.Lang.Python3/Instructions/JumpIfFalse.cs
@@ -1,17 +1,28 @@
-﻿using Zifro.Compiler.Core.Entities;
+﻿using System;
+using Zifro.Compiler.Core.Entities;
+using Zifro.Compiler.Core.Interfaces;
 using Zifro.Compiler.Lang.Python3.Interfaces;
 
 namespace Zifro.Compiler.Lang.Python3.Instructions
 {
     public class JumpIfFalse : Jump
     {
-        public JumpIfFalse(SourceReference source, Label label) : base(source, label)
+        public JumpIfFalse(SourceReference source, Label label)
+            : base(source, label)
         {
         }
 
         public override void Execute(PyProcessor processor)
         {
-            throw new System.NotImplementedException();
+            if (Label.OpCodeIndex == -1)
+                throw new InvalidOperationException("Label was not assigned an index. Are you sure it was added to the processor?");
+
+            var value = processor.PopValue<IScriptType>();
+
+            if (!value.IsTruthy())
+            {
+                processor.JumpToInstruction(Label.OpCodeIndex);
+            }
         }
     }
 }
