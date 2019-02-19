74b039ae06da5834694930cc622bb8da8a23d34c
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-19 16:24:35 +0100

    Added string multiplication with integers

--- src/Mellis.Lang.Python3/Entities/PyString.cs
+++ src/Mellis.Lang.Python3/Entities/PyString.cs
@@ -1,4 +1,5 @@
-﻿using Mellis.Core.Interfaces;
+﻿using System.Text;
+using Mellis.Core.Interfaces;
 using Mellis.Lang.Base.Entities;
 using Mellis.Lang.Python3.Syntax.Literals;
 
@@ -15,6 +16,27 @@ namespace Mellis.Lang.Python3.Entities
         public override IScriptType GetTypeDef()
         {
             throw new System.NotImplementedException();
+        }
+
+        public override IScriptType ArithmeticMultiply(IScriptType rhs)
+        {
+            switch (rhs)
+            {
+                case IntegerBase rhsInteger when rhsInteger.Value <= 0:
+                    return Processor.Factory.Create(string.Empty);
+
+                case IntegerBase rhsInteger:
+                    var builder = new StringBuilder(Value.Length * rhsInteger.Value);
+                    for (var i = 0; i < rhsInteger.Value; i++)
+                    {
+                        builder.Append(Value);
+                    }
+
+                    return Processor.Factory.Create(builder.ToString());
+
+                default:
+                    throw InvalidType(rhs, "*");
+            }
         }
 
         public override string ToString()
