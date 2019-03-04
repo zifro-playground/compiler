88bf3e81a31ffa63b72ee12c4c70618c76d79e79
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-04 11:03:59 +0100

    Added ClrYieldingFunction to Mellis

---
+++ src/Mellis/ClrYieldingFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Mellis.Core.Entities;
+using Mellis.Core.Interfaces;
+
+namespace Mellis
+{
+    public abstract class ClrYieldingFunction : IClrYieldingFunction
+    {
+        /// <param name="name">
+        /// The name of this function.
+        /// Used to identify it in the script environment and should therefore be a valid identifier.
+        /// <para>Name rules: <code>(letter | '_') (letter | '_' | number)*</code></para>
+        /// <para>Where <c>number</c> is <c>0-9</c></para>
+        /// <para>Where <c>letter</c> is defined by all Unicode letter categories: UppercaseLetter, LowercaseLetter, TitlecaseLetter, ModifierLetter, and OtherLetter.</para>
+        /// </param>
+        protected ClrYieldingFunction(string name)
+        {
+            FunctionName = name;
+        }
+
+        /// <inheritdoc />
+        public IProcessor Processor { get; set; }
+
+        /// <inheritdoc />
+        public string FunctionName { get; }
+
+        /// <inheritdoc />
+        public abstract void Invoke(IScriptType[] arguments);
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Should not be used.
+        /// You must call the yielding invoke <see cref="Invoke"/> on yielding functions.
+        /// </summary>
+        [Obsolete("You must call the yielding invoke on yielding functions.", true)]
+        IScriptType IClrFunction.Invoke(IScriptType[] arguments)
+        {
+            throw new NotSupportedException("You must call the yielding invoke on yielding functions.");
+        }
+    }
+}
