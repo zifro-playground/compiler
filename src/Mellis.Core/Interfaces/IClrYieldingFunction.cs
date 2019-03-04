99129103bb436e4ef2d04ac9e04b88174e5d163d
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-04 11:03:39 +0100

    Changed yielding function interface to inherit from iclrfunction.

--- src/Mellis.Core/Interfaces/IClrYieldingFunction.cs
+++ src/Mellis.Core/Interfaces/IClrYieldingFunction.cs
@@ -11,23 +11,8 @@ namespace Mellis.Core.Interfaces
     /// This can be turned into a value via <seealso cref="IScriptTypeFactory.Create(IClrYieldingFunction)"/>.
     /// </para>
     /// </summary>
-    public interface IClrYieldingFunction
+    public interface IClrYieldingFunction : IClrFunction
     {
-        /// <summary>
-        /// The assigned processor environment.
-        /// Set by the processor when function is created via the processors <see cref="IScriptTypeFactory"/> factory.
-        /// </summary>
-        IProcessor Processor { set; }
-
-        /// <summary>
-        /// Get the name of this function.
-        /// Used to identify it in the script environment and should therefore be a valid identifier.
-        /// <para>Name rules: <code>(letter | '_') (letter | '_' | number)*</code></para>
-        /// <para>Where <c>number</c> is <c>0-9</c></para>
-        /// <para>Where <c>letter</c> is defined by all Unicode letter categories: UppercaseLetter, LowercaseLetter, TitlecaseLetter, ModifierLetter, and OtherLetter.</para>
-        /// </summary>
-        string FunctionName { get; }
-
         /// <summary>
         /// Executed by the processor when the function is invoked in the script environment.
         /// <para>
@@ -36,6 +21,6 @@ namespace Mellis.Core.Interfaces
         /// to resume execution.
         /// </para>
         /// </summary>
-        void Invoke(IScriptType[] arguments);
+        new void Invoke(IScriptType[] arguments);
     }
 }
