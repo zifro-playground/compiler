4d5a7f3b0410032b0937223c2d405d800ef6d02e
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-18 13:26:06 +0100

    Added pyInteger tests

---
+++ src/Mellis.Lang.Python3.Tests/Entities/PyIntegerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Mellis.Lang.Base.Resources;
+using Mellis.Lang.Python3.Entities;
+using Mellis.Lang.Python3.Entities.Classes;
+using Mellis.Lang.Python3.Resources;
+using Mellis.Lang.Python3.VM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mellis.Lang.Python3.Tests.Entities
+{
+    [TestClass]
+    public class PyIntegerTests : BaseEntityTester<PyInteger, int>
+    {
+        protected override string ExpectedTypeName => Localized_Base_Entities.Type_Int_Name;
+        protected override Type ExpectedTypeDef => typeof(PyIntegerType);
+
+        protected override PyInteger CreateEntity(PyProcessor processor, int value)
+        {
+            return new PyInteger(processor, value, nameof(PyBooleanTests));
+        }
+
+        [DataTestMethod]
+        [DataRow(0, "0")]
+        [DataRow(+1, "1")]
+        [DataRow(-1, "-1")]
+        [DataRow(int.MaxValue, "2147483647")]
+        [DataRow(int.MinValue, "-2147483648")]
+        public override void ToStringDataTest(int value, string expected)
+        {
+            base.ToStringDataTest(value, expected);
+        }
+    }
+}
