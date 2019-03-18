85d50417fb97ae86fdf9823bee948ef6f503d125
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-18 12:27:57 +0100

    Added unit tests for None and CLR func base types

---
+++ src/Mellis.Lang.Python3.Tests/Entities/PyNoneTypeTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Mellis.Core.Entities;
+using Mellis.Core.Exceptions;
+using Mellis.Core.Interfaces;
+using Mellis.Lang.Base.Resources;
+using Mellis.Lang.Python3.Entities;
+using Mellis.Lang.Python3.Entities.Classes;
+using Mellis.Lang.Python3.Exceptions;
+using Mellis.Lang.Python3.Instructions;
+using Mellis.Lang.Python3.Interfaces;
+using Mellis.Lang.Python3.Resources;
+using Mellis.Lang.Python3.VM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mellis.Lang.Python3.Tests.Entities
+{
+    [TestClass]
+    public class PyNoneTypeTests : BaseEntityTypeTester<PyNoneType, PyNone>
+    {
+        protected override string ExpectedClassName => Localized_Base_Entities.Type_Null_Name;
+
+        protected override PyNoneType CreateEntity(PyProcessor processor)
+        {
+            return new PyNoneType(processor);
+        }
+
+        [TestMethod]
+        public void CtorCantUseIt()
+        {
+            // Arrange
+            var entity = CreateEntity();
+
+            void Action()
+            {
+                entity.Invoke(new IScriptType[0]);
+            }
+
+            // Act
+            var ex = Assert.ThrowsException<RuntimeException>((Action) Action);
+
+            // Assert
+            Assert.That.ErrorFormatArgsEqual(ex,
+                nameof(Localized_Python3_Runtime.Ex_Type_CannotInstantiate),
+                /* type name */ ExpectedClassName);
+        }
+    }
+}
