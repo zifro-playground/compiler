f1dc1c7dbc20e457b27017bd73d8b6438029dee6
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-18 13:27:09 +0100

    Extracted BaseEntityTester into two files

---
+++ src/Mellis.Lang.Python3.Tests/Entities/_BaseEntityTester`2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Mellis.Core.Interfaces;
+using Mellis.Lang.Python3.VM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mellis.Lang.Python3.Tests.Entities
+{
+    public abstract class BaseEntityTester<T, TInner>
+        : BaseEntityTester<T>
+        where T : IScriptType
+    {
+        protected abstract T CreateEntity(PyProcessor processor, TInner value);
+        protected virtual TInner DefaultValue => default;
+
+        protected T CreateEntity(TInner value)
+        {
+            return CreateEntity(new PyProcessor(), value);
+        }
+
+        protected override T CreateEntity(PyProcessor processor)
+        {
+            return CreateEntity(processor, DefaultValue);
+        }
+
+        public virtual void ToStringDataTest(TInner value, string expected)
+        {
+            // Arrange
+            var entity = CreateEntity(value);
+
+            // Act
+            var result = entity.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, result, $"{typeof(T).Name}.ToString() returned wrong value.");
+        }
+    }
+}
