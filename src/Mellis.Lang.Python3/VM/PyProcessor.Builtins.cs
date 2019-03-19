7ed502539374de7027cee5e4004a7a55cbb502e7
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-19 16:23:14 +0100

    Updated PyProcessor to employ IProcessor.AddBuiltins(IEmbeddedType), upping version to 0.3.2

--- src/Mellis.Lang.Python3/VM/PyProcessor.Builtins.cs
+++ src/Mellis.Lang.Python3/VM/PyProcessor.Builtins.cs
@@ -1,4 +1,5 @@
-﻿using Mellis.Core.Interfaces;
+﻿using System;
+using Mellis.Core.Interfaces;
 using Mellis.Lang.Python3.Entities;
 using Mellis.Lang.Python3.Entities.Classes;
 
@@ -6,11 +7,28 @@ namespace Mellis.Lang.Python3.VM
 {
     public partial class PyProcessor
     {
-        public void AddBuiltin(params IClrFunction[] builtinList)
+        public void AddBuiltin(params IEmbeddedType[] builtinList)
         {
-            foreach (IClrFunction function in builtinList)
+            foreach (IEmbeddedType embeddedType in builtinList)
             {
-                _builtins.SetVariable(function.FunctionName, Factory.Create(function));
+                _builtins.SetVariable(
+                    embeddedType.FunctionName,
+                    EmbeddedTypeToScriptType(embeddedType));
+            }
+
+            IScriptType EmbeddedTypeToScriptType(IEmbeddedType embedded)
+            {
+                switch (embedded)
+                {
+                case IClrFunction function:
+                    return Factory.Create(function);
+
+                case IClrYieldingFunction yieldingFunction:
+                    return Factory.Create(yieldingFunction);
+
+                default:
+                    throw new ArgumentException();
+                }
             }
         }
 
@@ -20,7 +38,7 @@ namespace Mellis.Lang.Python3.VM
                 // Base types
                 new PyDoubleType(this, "float"),
                 new PyIntegerType(this, "int"),
-                new PyStringType(this, "str"), 
+                new PyStringType(this, "str"),
                 new PyBooleanType(this, "bool"),
                 new PyType(this, "type"),
 
