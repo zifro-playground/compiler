aaf993dc31912613b890993e26086c4933e393a4
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-20 11:25:28 +0100

    Resolved units for builtin iter func

--- src/Mellis.Lang.Python3/Entities/Functions/Next.cs
+++ src/Mellis.Lang.Python3/Entities/Functions/Next.cs
@@ -1,4 +1,8 @@
-﻿using Mellis.Core.Interfaces;
+﻿using System.Collections.Generic;
+using Mellis.Core.Exceptions;
+using Mellis.Core.Interfaces;
+using Mellis.Lang.Python3.Exceptions;
+using Mellis.Lang.Python3.Resources;
 
 namespace Mellis.Lang.Python3.Entities.Functions
 {
@@ -10,7 +14,53 @@ namespace Mellis.Lang.Python3.Entities.Functions
 
         public override IScriptType Invoke(params IScriptType[] arguments)
         {
-            throw new System.NotImplementedException();
+            if (arguments.Length > 2)
+            {
+                throw new RuntimeTooManyArgumentsException(FunctionName, 2, arguments.Length);
+            }
+            if (arguments.Length == 0)
+            {
+                throw new RuntimeTooFewArgumentsException(FunctionName, 1, arguments.Length);
+            }
+
+            if (arguments.Length == 2)
+            {
+            }
+
+            var value = arguments[0];
+
+            if (!(value is IEnumerator<IScriptType> enumerator))
+            {
+                throw new RuntimeException(
+                    nameof(Localized_Python3_Entities.Builtin_Next_Arg1_NotIterator),
+                    Localized_Python3_Entities.Builtin_Next_Arg1_NotIterator,
+                    value.GetTypeName()
+                );
+            }
+
+            if (enumerator.MoveNext())
+            {
+                return enumerator.Current;
+            }
+
+            if (arguments.Length == 2)
+            {
+                return arguments[1];
+            }
+
+            if (!(value.Name is null))
+            {
+                throw new RuntimeException(
+                    nameof(Localized_Python3_Entities.Builtin_Next_StopIteration_Named),
+                    Localized_Python3_Entities.Builtin_Next_StopIteration_Named,
+                    value.Name
+                );
+            }
+
+            throw new RuntimeException(
+                nameof(Localized_Python3_Entities.Builtin_Next_StopIteration),
+                Localized_Python3_Entities.Builtin_Next_StopIteration
+            );
         }
     }
 }
