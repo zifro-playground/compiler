7a7f7fb3a28fcb6197fcea5a843ccdc10266bd5c
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-20 16:46:47 +0100

    Resolved PyRange units

--- src/Mellis.Lang.Python3/Entities/PyRange.cs
+++ src/Mellis.Lang.Python3/Entities/PyRange.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Mellis.Core.Interfaces;
 using Mellis.Lang.Base.Entities;
+using Mellis.Lang.Python3.Entities.Classes;
+using Mellis.Lang.Python3.Resources;
 
 namespace Mellis.Lang.Python3.Entities
 {
@@ -27,37 +29,92 @@ namespace Mellis.Lang.Python3.Entities
 
         public override IScriptType Copy(string newName)
         {
-            throw new NotImplementedException();
+            return new PyRange(Processor, RangeFrom, RangeTo, RangeStep, newName);
         }
 
         public override IScriptType GetTypeDef()
         {
-            throw new NotImplementedException();
+            return new PyRangeType(Processor);
         }
 
         public override string GetTypeName()
         {
-            throw new NotImplementedException();
+            return Localized_Python3_Entities.Type_Range_Name;
         }
 
         public override bool IsTruthy()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public override bool TryConvert(Type type, out object value)
         {
-            throw new NotImplementedException();
+            if (type == typeof(IEnumerable<IScriptType>))
+            {
+                value = this;
+                return true;
+            }
+
+            if (type == typeof(IEnumerable))
+            {
+                value = this;
+                return true;
+            }
+
+            if (type == typeof(IEnumerator<IScriptType>))
+            {
+                value = GetEnumerator();
+                return true;
+            }
+
+            if (type == typeof(IEnumerator))
+            {
+                value = ((IEnumerable)this).GetEnumerator();
+                return true;
+            }
+
+            value = default;
+            return false;
         }
 
         public IEnumerator<IScriptType> GetEnumerator()
         {
-            throw new NotImplementedException();
+            if (RangeStep > 0)
+            {
+                for (int i = RangeFrom; i < RangeTo; i += RangeStep)
+                {
+                    yield return Processor.Factory.Create(i);
+                }
+            }
+
+            if (RangeStep < 0)
+            {
+                for (int i = RangeFrom; i > RangeTo; i += RangeStep)
+                {
+                    yield return Processor.Factory.Create(i);
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            if (RangeStep == 1)
+            {
+                return string.Format(
+                    Localized_Python3_Entities.Type_Range_ToString,
+                    RangeFrom, RangeTo
+                );
+            }
+
+            return string.Format(
+                Localized_Python3_Entities.Type_Range_ToString_Step,
+                RangeFrom, RangeTo, RangeStep
+            );
         }
     }
 }
