7f12b7e1aae0560162a67a86cc8edffb079a03f6
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-20 15:37:33 +0100

    Added range definition variables: to, from, & step

--- src/Mellis.Lang.Python3.Tests/Entities/PyRangeTests.cs
+++ src/Mellis.Lang.Python3.Tests/Entities/PyRangeTests.cs
@@ -6,15 +6,21 @@ using Mellis.Lang.Python3.VM;
 
 namespace Mellis.Lang.Python3.Tests.Entities
 {
-    public class PyRangeTests : BaseEntityTester<PyRange>
+    public class PyRangeTests : BaseEntityTester<PyRange, (int from, int to, int step)>
     {
-        protected override string ExpectedTypeName => Localized_Python3_Entities.Type_Range_Name;
+        protected override string ExpectedTypeName
+            => Localized_Python3_Entities.Type_Range_Name;
 
-        protected override Type ExpectedTypeDef => typeof(PyRangeType);
+        protected override Type ExpectedTypeDef
+            => typeof(PyRangeType);
 
-        protected override PyRange CreateEntity(PyProcessor processor)
+        protected override (int from, int to, int step) DefaultValue
+            => (from: 0, to: 10, step: 1);
+
+        protected override PyRange CreateEntity(PyProcessor processor, (int from, int to, int step) value)
         {
-            return new PyRange(processor, nameof(PyRangeTests));
+            (int from, int to, int step) = value;
+            return new PyRange(processor, from, to, step, nameof(PyRangeTests));
         }
     }
 }
--- src/Mellis.Lang.Python3/Entities/PyRange.cs
+++ src/Mellis.Lang.Python3/Entities/PyRange.cs
@@ -6,9 +6,21 @@ namespace Mellis.Lang.Python3.Entities
 {
     public class PyRange : ScriptTypeBase
     {
-        public PyRange(IProcessor processor, string name = null)
+        public int RangeFrom { get; }
+        public int RangeTo { get; }
+        public int RangeStep { get; }
+
+        public PyRange(
+            IProcessor processor,
+            int rangeFrom,
+            int rangeTo,
+            int rangeStep = 1,
+            string name = null)
             : base(processor, name)
         {
+            RangeFrom = rangeFrom;
+            RangeTo = rangeTo;
+            RangeStep = rangeStep;
         }
 
         public override IScriptType Copy(string newName)
