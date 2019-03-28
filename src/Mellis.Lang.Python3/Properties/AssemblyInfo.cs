af610a5f3c195abe35ad9035dc846f55727e9683
kalle (jag) <kalle.jillheden@gmail.com>
2019-03-28 10:52:02 +0100

    Removed signing of Mellis.Lang.Python3.Tests project Since sigining was disabled on all other projects. Gotta sign all or none

--- src/Mellis.Lang.Python3/Properties/AssemblyInfo.cs
+++ src/Mellis.Lang.Python3/Properties/AssemblyInfo.cs
@@ -5,9 +5,10 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Mellis.Lang.Python3.Tests, PublicKey=" +
-                             "0024000004800000940000000602000000240000525341310004000001000100d92cc51fd864e4" +
-                             "18732db9008adef773ff8f8bc636d31236f7d3020871eccc1251688930576ea3aee2ac172193a8" +
-                             "e02dac96bf53982b9ad5e25baae56479c9e982088cf9dd3563b456bf3e7550f69e4d288edb784e" +
-                             "a3b8cb10d20ed74c3dc70e1190374cea0d9eecb03d9c0b8b0242b87de670594203a2c45d9f652b" +
-                             "70cd82b0")] 
+[assembly:InternalsVisibleTo("Mellis.Lang.Python3.Tests")]
+//[assembly:InternalsVisibleTo("Mellis.Lang.Python3.Tests, PublicKey=" +
+//                             "0024000004800000940000000602000000240000525341310004000001000100d92cc51fd864e4" +
+//                             "18732db9008adef773ff8f8bc636d31236f7d3020871eccc1251688930576ea3aee2ac172193a8" +
+//                             "e02dac96bf53982b9ad5e25baae56479c9e982088cf9dd3563b456bf3e7550f69e4d288edb784e" +
+//                             "a3b8cb10d20ed74c3dc70e1190374cea0d9eecb03d9c0b8b0242b87de670594203a2c45d9f652b" +
+//                             "70cd82b0")] 
