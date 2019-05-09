2b3e57365805dfe4a16ee2cc6ae0440236696ac7
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-09 15:15:43 +0200

    Added IndexGet to PyString & fixed str*int commutation

--- src/Mellis.Tests/Entities/ScriptStringTests.cs
+++ src/Mellis.Tests/Entities/ScriptStringTests.cs
@@ -35,63 +35,6 @@ namespace Mellis.Tests.Entities
             AssertArithmeticResult<ScriptString>(result, a, b, "");
         }
 
-        [TestMethod]
-        public void IndexGetInteger()
-        {
-            // Arrange
-            var a = GetScriptString("foo");
-            var b = GetScriptInteger(0);
-
-            // Act
-            var result = a.GetIndex(b);
-
-            // Assert
-            AssertArithmeticResult<ScriptString>(result, a, b, "f");
-        }
-
-        [TestMethod]
-        public void IndexGetIntegerOutOfRange()
-        {
-            // Arrange
-            var a = GetScriptString("foo");
-            var b = GetScriptInteger(10);
-            object[] expectedFormatArgs = {a.Value, a.Value.Length, b.Value};
-
-            void Action()
-            {
-                a.GetIndex(b);
-            }
-
-            // Act + Assert
-            AssertThrow(Action,
-                nameof(Localized_Base_Entities.Ex_String_IndexGet_OutOfRange),
-                expectedFormatArgs);
-        }
-
-        [TestMethod]
-        public void IndexGetInvalid()
-        {
-            // Arrange
-            var a = GetScriptString("foo");
-            var b = GetScriptString("a");
-            object[] expectedFormatArgs = {a.Value, a.Value.Length, b.GetTypeName()};
-
-            void Action()
-            {
-                a.GetIndex(b);
-            }
-
-            // Act + Assert
-            AssertThrow(Action,
-                nameof(Localized_Base_Entities.Ex_String_IndexGet_InvalidType),
-                expectedFormatArgs);
-        }
-
-        public override void IndexGet_NotImplemented()
-        {
-            // disabled
-        }
-
         protected override IScriptType GetBasicOperand()
         {
             return GetScriptString("foo");
