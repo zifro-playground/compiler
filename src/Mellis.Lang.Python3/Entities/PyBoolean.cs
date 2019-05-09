a8a27d9b5faa02e79c5ef5c435c35a75344ba429
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-09 17:14:03 +0200

    Fixed PyBoolean type name & to use PyInteger.Value to evaluate bool, & upped p->v0.8.5

--- src/Mellis.Lang.Python3/Entities/PyBoolean.cs
+++ src/Mellis.Lang.Python3/Entities/PyBoolean.cs
@@ -9,17 +9,21 @@ namespace Mellis.Lang.Python3.Entities
 {
     public class PyBoolean : PyInteger, IScriptBoolean
     {
-        public new bool Value { get; }
+        public new bool Value => base.Value != 0;
 
         public PyBoolean(IProcessor processor, bool value)
             : base(processor, value ? 1 : 0)
         {
-            Value = value;
         }
 
         public override IScriptType GetTypeDef()
         {
             return new PyBooleanType(Processor);
+        }
+
+        public override string GetTypeName()
+        {
+            return Localized_Base_Entities.Type_Boolean_Name;
         }
 
         public override string ToString()
