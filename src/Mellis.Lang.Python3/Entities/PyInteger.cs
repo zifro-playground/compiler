51159395dc15e6c229abe2c849ae2d7a35683ff3
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-09 15:16:47 +0200

    Added all python-specific operations to PyBoolean & PyInteger

--- src/Mellis.Lang.Python3/Entities/PyInteger.cs
+++ src/Mellis.Lang.Python3/Entities/PyInteger.cs
@@ -1,6 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using Mellis.Core.Interfaces;
 using Mellis.Lang.Python3.Entities.Classes;
+using Mellis.Tools;
 
 namespace Mellis.Lang.Python3.Entities
 {
@@ -19,6 +21,38 @@ namespace Mellis.Lang.Python3.Entities
         public override string ToString()
         {
             return Value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public override IScriptType ArithmeticExponent(IScriptType rhs)
+        {
+            switch (rhs)
+            {
+            case ScriptInteger i when i.Value >= 0:
+                // Try integer exponentiation
+                return Processor.Factory.Create(MathUtilities.Pow(Value, i.Value));
+            case ScriptInteger i:
+                return Processor.Factory.Create(Math.Pow(Value, i.Value));
+            case ScriptDouble d:
+                return Processor.Factory.Create(Math.Pow(Value, d.Value));
+            default:
+                return null;
+            }
+        }
+
+        public override IScriptType ArithmeticExponentReverse(IScriptType lhs)
+        {
+            switch (lhs)
+            {
+            case ScriptInteger i when Value >= 0:
+                // Try integer exponentiation
+                return Processor.Factory.Create(MathUtilities.Pow(i.Value, Value));
+            case ScriptInteger i:
+                return Processor.Factory.Create(Math.Pow(i.Value, Value));
+            case ScriptDouble d:
+                return Processor.Factory.Create(Math.Pow(d.Value, Value));
+            default:
+                return null;
+            }
         }
     }
 }
